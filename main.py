0c22ee60568a25fe71af8ce17b60685e73340ff4
nishithar <102227209+nishithar@users.noreply.github.com>
2022-03-23 15:42:26 -0500

    adding the two values show addition

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+    # Use a breakpoint in the code line below to debug your script.
+
+
+      # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+x=int(input("X"));
+y=int(input("Y"));
+print("x+y = ", (x+y));
